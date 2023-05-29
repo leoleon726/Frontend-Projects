5506f571b6adae87a70c0abc9387f81e1a6d876d
Leonardo Leon <leoleon726@gmail.com>
2023-05-29 22:23:01 +0200

    integrate with firebase and have login page

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -5,18 +5,46 @@ import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import { AppTopbarRef } from '../types/types';
 import { LayoutContext } from './context/layoutcontext';
+import { Menu } from 'primereact/menu';
+import { useRouter } from 'next/router';
+import { useAuth } from '../context/AuthContext';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
     const topbarmenubuttonRef = useRef(null);
+    const menu = useRef<Menu>(null);
+    const { user, logout } = useAuth()
+    const router = useRouter()
+    const toggleMenu: React.MouseEventHandler<HTMLButtonElement> | undefined = (event) => {
+        menu.current?.toggle(event);
+    };
 
     useImperativeHandle(ref, () => ({
         menubutton: menubuttonRef.current,
         topbarmenu: topbarmenuRef.current,
         topbarmenubutton: topbarmenubuttonRef.current
     }));
+    const loginItems = [
+        {
+            label: 'Cerrar sesion',
+            icon: 'pi pi-sign-out',
+            command: () => {
+                logout()
+                router.push('/auth/login')
+            }
+        }
+    ];
+    const notLoginItems = [
+        {
+            label: 'Iniciar Sesion',
+            icon: 'pi pi-sign-in',
+            command: () => {
+                router.push('/auth/login')
+            }
+        }
+    ];
 
     return (
         <div className="layout-topbar">
@@ -34,16 +62,22 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
             </button>
 
             <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
-                <button type="button" className="p-link layout-topbar-button">
-                    <i className="pi pi-user"></i>
-                    <span>Profile</span>
-                </button>
-                <Link href="/documentation">
-                    <button type="button" className="p-link layout-topbar-button">
-                        <i className="pi pi-cog"></i>
-                        <span>Settings</span>
-                    </button>
-                </Link>
+
+                {user ? (
+                    <>
+                        <Menu ref={menu} model={loginItems} popup /><button type="button" className="p-link layout-topbar-button" onClick={toggleMenu}>
+                            <i className="pi pi-user"></i>
+                            <span>Profile</span>
+                        </button>
+                    </>) : (
+                    <>
+                        <Menu ref={menu} model={notLoginItems} popup /><button type="button" className="p-link layout-topbar-button" onClick={toggleMenu}>
+                            <i className="pi pi-user"></i>
+                            <span>Profile</span>
+                        </button>
+                    </>
+                )}
+
             </div>
         </div>
     );
